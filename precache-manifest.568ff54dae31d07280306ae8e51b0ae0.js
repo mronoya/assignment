72e6abfb65ab061af099ac660e9d736a78e6199f
mronoya <mronoya@hotmail.com>
2020-09-11 22:10:22 +0200

    Updates

---
+++ precache-manifest.568ff54dae31d07280306ae8e51b0ae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad9c495d35cc75de80f83fc99bafb37",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "2376e285da87e6dd1487",
+    "url": "/assignment/static/css/main.8008d301.chunk.css"
+  },
+  {
+    "revision": "166e8ad82de51ba0efd4",
+    "url": "/assignment/static/js/2.73c129d7.chunk.js"
+  },
+  {
+    "revision": "2376e285da87e6dd1487",
+    "url": "/assignment/static/js/main.981cc7e0.chunk.js"
+  },
+  {
+    "revision": "cb04951a33e045aed6e1",
+    "url": "/assignment/static/js/runtime~main.bf340767.js"
+  },
+  {
+    "revision": "550eff998a52393fc4df264ecd54b4d1",
+    "url": "/assignment/static/media/cross.550eff99.svg"
+  },
+  {
+    "revision": "12a3c00ee3253b528f52a1d7ce4e92bf",
+    "url": "/assignment/static/media/magnifying-glass.12a3c00e.svg"
+  }
+]);
